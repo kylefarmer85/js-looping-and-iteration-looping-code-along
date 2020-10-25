9f143ca7b5ab278ecad649fda9aaf424d3841237
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-25 17:07:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(name, birthday) {
+  let array = []
+  for (let i = 0; i < name.length; i++)
+    array.push(`Thank you, ${name[i]}, for the wonderful ${birthday} gift!`)
+  return array
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
